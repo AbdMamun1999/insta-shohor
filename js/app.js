137532d3d6e32abd5f646955617986864a5a6895
AbdMamun1999 <mamunrox@gmail.com>
2022-03-11 10:54:50 +0600

    fix corner report section double data problem

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ const isLiked = (id) => {
 const addToLiked = (id) => {
     likedPostsId.push(id); 
     showPosts(posts);
+    console.log(posts)
 };
 
 const reportPost = (id) => {
@@ -144,7 +145,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
-    document.getElementById('liked').textContent = ' '
+    document.getElementById('liked').textContent = ' ';
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
@@ -153,6 +154,7 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+    document.getElementById('reported').textContent = '';
     const reportedPosts = getReportedPosts();
     reportedPosts.forEach((post) => {
         const div = createPost(post);
